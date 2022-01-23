90f6c16a7f969ddb5633925ded5f748c88877249
fallsantos <fallsantosdev@hotmail.com>
2022-01-23 01:57:37 -0300

    :bug: fix api request url

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -27,7 +27,7 @@ class Feed extends Component {
     }
 
     registerToSocket = () => {
-        const socket = io('https://fausantosdev-share-api.herokuapp.com')
+        const socket = io(process.env.REACT_APP_API_URL)
 
         socket.on('post', newPost => {
             this.setState({ feed: [newPost, ...this.state.feed] })
